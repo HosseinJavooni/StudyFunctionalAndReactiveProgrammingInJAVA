9d41179912161115aaceb75901b8bdf1c99335c5
Hossein Jabani <h.javooni@faraboom.co>
2024-07-17 15:10:41 +0330

    master_‫چارشنبه ۱۷ جولای ۲۴، ساعات ۱۵:۱۰:۴۱ (+0330)‬

--- src/main/java/passBehaviourAsAParameterToAMethod/PassBehaviourAsAParameterToAMethod.java
+++ src/main/java/passBehaviourAsAParameterToAMethod/PassBehaviourAsAParameterToAMethod.java
@@ -7,12 +7,15 @@ public class PassBehaviourAsAParameterToAMethod {
 	public static void main(String[] args) {
 //		MyFunInterface fun = () -> System.out.println("Hello");
 //		fun.doSomething();
-		// pass the behavior to a method
-		followBehaviour(() -> System.out.println("This is a behaviour"));
-		followBehaviour(() -> {
+		// pass the behavior to a method like a parameter:
+		MyFunInterface behaviour1 = () -> System.out.println("This is a behaviour");
+		MyFunInterface behaviour2 = () -> {
 			Random random = new Random();
 			System.out.println(random.nextInt(1000) + " -> Another behavior");
-			});
+			};
+			
+		followBehaviour(behaviour1);
+		followBehaviour(behaviour2);
 	}
 	
 	public static void followBehaviour(MyFunInterface fun) {
